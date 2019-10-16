08476b0158e73d003a760b53ec6d5bf21e50d2e1
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 17:51:23 +0200

    Updates

---
+++ precache-manifest.ef08226b04b12098d89447791ae09960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6677184cb9dce565b91a",
+    "url": "/FormValidation_React/static/js/runtime~main.6677184c.js"
+  },
+  {
+    "revision": "957a7936fdc00a4948d5",
+    "url": "/FormValidation_React/static/js/main.957a7936.chunk.js"
+  },
+  {
+    "revision": "0c611968a9fee242c72c",
+    "url": "/FormValidation_React/static/js/1.0c611968.chunk.js"
+  },
+  {
+    "revision": "957a7936fdc00a4948d5",
+    "url": "/FormValidation_React/static/css/main.dd6c5aa5.chunk.css"
+  },
+  {
+    "revision": "602e68f240968ea236eea2ac1459b161",
+    "url": "/FormValidation_React/index.html"
+  }
+];
